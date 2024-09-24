589024e50727a60ee23a3372da5b3e8866834de1
chxni <jcg3098@naver.com>
2024-09-24 20:15:30 +0900

    init : 프로젝트 초기 세팅

---
+++ src/main/java/com/coders/boardgame/BoardgameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coders.boardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgameBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardgameBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coders/boardgame/BoardgameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coders.boardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardgameBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
