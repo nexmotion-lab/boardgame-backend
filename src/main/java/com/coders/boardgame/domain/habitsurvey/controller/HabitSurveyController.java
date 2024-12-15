51eedfeebe938869ba910cba694b013537f6bb26
chxni <jcg3098@naver.com>
2024-12-15 17:25:28 +0900

    [refactor] 데이터 매핑 로직 수정
    - 프론트엔드에서 전달되는 JSON 구조 변경 반영

--- src/main/java/com/coders/boardgame/domain/habitsurvey/controller/HabitSurveyController.java
+++ src/main/java/com/coders/boardgame/domain/habitsurvey/controller/HabitSurveyController.java
@@ -1,8 +1,10 @@
 package com.coders.boardgame.domain.habitsurvey.controller;
 
+import com.coders.boardgame.domain.habitsurvey.dto.HabitSurveyResultDto;
 import com.coders.boardgame.domain.habitsurvey.entity.HabitSurvey;
 import com.coders.boardgame.domain.habitsurvey.service.HabitSurveyService;
-import com.coders.boardgame.domain.habitsurvey.dto.SurveySelectedOptionDto;
+import com.coders.boardgame.exception.auth.CustomSessionAuthenticationException;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +24,21 @@ public class HabitSurveyController {
     private final HabitSurveyService habitSurveyService;
 
     @GetMapping
-    public List<HabitSurvey> getAllSurveys() {
-        return habitSurveyService.getAllSurveys();
+    public ResponseEntity<List<HabitSurvey>> getAllSurveys() {
+        List<HabitSurvey> surveyList = habitSurveyService.getAllSurveys();
+        return ResponseEntity.ok(surveyList);
     }
 
     @PostMapping("/results")
-    public ResponseEntity<Void> saveSurveyResult(@RequestParam Long userId, @RequestBody List<SurveySelectedOptionDto> selectedOptions) {
-        habitSurveyService.saveSurveyResult(userId, selectedOptions);
+    public ResponseEntity<Void> saveSurveyResult(@RequestBody HabitSurveyResultDto habitSurveyResult, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+
+        if (userId == null) {
+            log.warn("사용자 ID가 없습니다. userId: " + userId);
+            throw new CustomSessionAuthenticationException("사용자 ID가 없습니다.");
+        }
+
+        habitSurveyService.saveSurveyResult(userId, habitSurveyResult);
         return ResponseEntity.ok().build();
     }
 }
