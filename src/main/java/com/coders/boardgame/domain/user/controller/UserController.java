f114119ceb03d29219141ed2ef8589a49ecc8f5e
DongHyeon Yoon <128972134+donghyeon0619@users.noreply.github.com>
2024-12-17 20:09:48 +0900

    [edit]: exceptionHandler 수정

--- src/main/java/com/coders/boardgame/domain/user/controller/UserController.java
+++ src/main/java/com/coders/boardgame/domain/user/controller/UserController.java
@@ -3,13 +3,13 @@ package com.coders.boardgame.domain.user.controller;
 import com.coders.boardgame.domain.user.dto.UserDto;
 import com.coders.boardgame.domain.user.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,5 +30,24 @@ public class UserController {
             // 3. 예외 처리
             return ResponseEntity.status(500).body("An error occurred during signup: " + e.getMessage());
         }
+    }
+
+    /**
+     * 세션 유효성 확인 API
+     * @param request HttpServletRequest
+     * @return 세션 유효 상태를 반환
+     */
+    @GetMapping("/session")
+    public ResponseEntity<String> checkSessionStatus(HttpServletRequest request){
+
+        // HttpSession에서 세션 확인
+        HttpSession session = request.getSession(false); // 세션이 없으면 null 반환
+
+        if (session != null && session.getAttribute("userId") != null) {
+            return ResponseEntity.ok("로그인 완료");
+        } else {
+            return ResponseEntity.status(401).body("로그인이 되어있지 않습니다.");
+        }
+
     }
 }
--- src/main/java/com/coders/boardgame/domain/user/service/SessionService.java
+++ src/main/java/com/coders/boardgame/domain/user/service/SessionService.java
@@ -26,4 +26,5 @@ public class SessionService {
 
         return userId;
     }
+
 }
